f2f2ecdacc5653b7e9a25cb8f535430b0ca1e70a
Mridul11 <mridulmishra2991@gmail.com>
2018-04-06 02:15:26 +0530

    first commit

---
+++ functions/index.js
@@ -0,0 +1,11 @@
+const admin = requre('firebase-admin');
+const functions = require('firebase-functions');
+const createUser = require('./create_user');
+const serviceAccount = require('./service_account.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://anjaliarch-b6970.firebaseio.com"
+});
+
+exports.createUser = functions.https.onRequest(createUser);
